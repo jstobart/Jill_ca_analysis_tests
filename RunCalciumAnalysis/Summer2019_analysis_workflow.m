19f226d775134f9f7ce99e5e91dbfedef33c7c82
jstobart <jill.stobart@gmail.com>
2019-06-09 22:29:19 -0500

    summer 2019 workflow

--- RunCalciumAnalysis/Summer2019_analysis_workflow.m
+++ RunCalciumAnalysis/Summer2019_analysis_workflow.m
@@ -3,6 +3,8 @@
 % RCaMP mice
 
 % All images: unmix RCaMP and FITC dextran
+ImgArray= ImgArray.unmix_chs(false, [], cell2mat(RCaMP_mGCaMP_Matrix));
+ImgArray = ImgArray.motion_correct('refImg',refImg, 'ch',2,'maxShift', 10,'minCorr', 0.4);%,'doPlot',true);
 
 
 % measure velocity line scan
