c07e5ed8e805b14e20e60edcc18e82933e8a09be
jstobart <jill.stobart@gmail.com>
2019-06-20 22:43:42 -0500

    process different whisker stim frequencies

--- RunCalciumAnalysis/2019/PericyteRCaMP_2019.m
+++ RunCalciumAnalysis/2019/PericyteRCaMP_2019.m
@@ -6,8 +6,9 @@ close all; clear all;
 Settings.ResultsFolder = 'D:\Data\test';
 
 Settings.FileNames = {  % folder names where images and roiSet.zip is found
+    'J:\Jill_Stobart\In_vivo_2P_Data\66678_Crazy8\2019_06_14\0Hz\spot1-frequenices-TSeries-06142019-1038-009',...
     'J:\Jill_Stobart\In_vivo_2P_Data\66678_Crazy8\2019_06_14\40Hz\spot1-frequenices-TSeries-06142019-1038-012',...
-    % etc.
+     % etc.
     };
 
 Settings.Baseline = 2; % time (s) before the whisker stimulator starts,  2s for short trials (10s long), 5s for long trials
@@ -35,27 +36,14 @@ for iFile= 1:length(Settings.FileNames)
     BL_frames= Settings.Baseline*ImgArray.metadata.frameRate; % number of baseline frames
     
     %% Configs for Lck GCaMP Cell Scan
-    %ASTROCYTES
+     % Pericyte RCaMP
     % 2D automated selection for peaks
-    AC_findConf = ConfigFindROIsFLIKA_2D.from_preset('ca_memb_astro', 'baselineFrames',...
+    findConf1 = ConfigFindROIsFLIKA_2D.from_preset('ca_neuron', 'baselineFrames',...
         BL_frames,'freqPassBand',0.5,'sigmaXY', 2,...
         'sigmaT', 0.14,'thresholdPuff', 7, 'threshold2D', 0.1,...
         'minRiseTime',0.14, 'maxRiseTime', 1,'minROIArea', 10,...
         'dilateXY', 4, 'dilateT', 0.3,'erodeXY', 1, 'erodeT', 0.1);
     
-    % measure ROIs (extract the traces)
-    AC_measureConf = ConfigMeasureROIsDummy();
-    
-    % filter the traces to detect the peaks and get info about them
-    AC_detectConf = ConfigDetectSigsClsfy('baselineFrames', BL_frames,...
-        'propagateNaNs', false, 'excludeNaNs', false, 'lpWindowTime', 1.5, 'spFilterOrder', 2,...
-        'spPassBandMin',0.05, 'spPassBandMax', 0.5, 'thresholdLP', 3,'thresholdSP', 5);
-    
-    % Combine the configs into a CellScan config for membrane tagged GCaMP
-    AC_configCS= ConfigCellScan(AC_findConf, AC_measureConf, AC_detectConf); %
-    
-    %% Configs for neuronal RCaMP Cell Scan
-    % NEURONS
     % hand selected- peaks from cellular structures
     x_pix= size(ImgArray(1,1).rawdata,2); y_pix= size(ImgArray(1,1).rawdata,1);
     scaleF = 1;
@@ -63,45 +51,49 @@ for iFile= 1:length(Settings.FileNames)
     zipfiles = dir(fullfile(testRoot,'*.zip'));  % look for the zip ROI file
     fnTempList2 = {zipfiles(:).name};
     zipPath = fullfile(testRoot, fnTempList2);
-    
+       
     % load image J ROIs for mask
-    N_findConf = ConfigFindROIsDummy.from_ImageJ(zipPath{1,1}, x_pix, y_pix, scaleF);
+    findConf2 = ConfigFindROIsDummy.from_ImageJ(zipPath{1,1}, x_pix, y_pix, scaleF);
+    
     
     % measure ROIs (extract the traces)
-    N_measureConf = ConfigMeasureROIsDummy();
+        measureConf = ConfigMeasureROIsDummy('baselineFrames', BL_frames);
+        
+    % filter the traces to detect the peaks and get info about them    
+        detectConf = ConfigDetectSigsClsfy('baselineFrames', BL_frames,...
+        'propagateNaNs', false, 'excludeNaNs', false, 'lpWindowTime', 1.5, 'spFilterOrder', 2,...
+        'spPassBandMin',0.05, 'spPassBandMax', 0.5, 'thresholdLP', 3,'thresholdSP', 5);
     
-    % filter the traces to detect the peaks and get info about them
-    N_detectConf = ConfigDetectSigsClsfy('baselineFrames', BL_frames,...
-        'propagateNaNs', false,'excludeNaNs', false, 'lpWindowTime', 5, 'spFilterOrder', 2,...
-        'spPassBandMin',0.1, 'spPassBandMax', 1, 'thresholdLP', 5,'thresholdSP', 4);
+    % Combine the configs into a CellScan config for automated ROI
+    % selection
+    configCS1= ConfigCellScan(findConf1, measureConf, detectConf); %
     
-    
-    % Combine the configs into a CellScan config for neuronal RCaMP
-    N_configCS= ConfigCellScan(N_findConf, N_measureConf, N_detectConf); %
+    % Combine the configs into a CellScan config for ImageJ ROIs
+    configCS2= ConfigCellScan(findConf2, measureConf, detectConf); %
     
     
     %% Create CellScan objects
-    astrocytes = CellScan(fnList, ImgArray, AC_configCS, 2); % peaks from automated ROIs
-    neurons = CellScan(fnList, ImgArray, N_configCS, 1); % peaks from hand clicked ROIs
+    automatedROIs = CellScan(fnList, ImgArray, configCS1, 1); % peaks from automated ROIs
+    ImageJROIs = CellScan(fnList, ImgArray, configCS2, 1); % peaks from hand clicked ROIs
     
     % Process the images
-    astrocytes =astrocytes.process();
-    neurons =neurons.process();
+    automatedROIs =automatedROIs.process();
+    ImageJROIs =ImageJROIs.process();
     
     % Make the debugging plots
-    astrocytes.plot();
-    neurons.plot();
+    automatedROIs.plot();
+    ImageJROIs.plot();
     
-    %astrocytes.opt_config()
-    %neurons.opt_config()
-    %neurons.plot('signals');
+    %automatedROIs.opt_config()
+    %ImageJROIs.opt_config()
+    %ImageJROIs.plot('signals');
     
     %% Output data
     OutputFile1=fullfile(Settings.ResultsFolder, strcat('Astrocytes_FieldofView',num2str(iFile)));
     OutputFile2=fullfile(Settings.ResultsFolder, strcat('Neurons_FieldofView',num2str(iFile)));
     
-    astrocytes.output_data(OutputFile1)
-    neurons.output_data(OutputFile2)
+    automatedROIs.output_data(OutputFile1)
+    ImageJROIs.output_data(OutputFile2)
     
 end
 
